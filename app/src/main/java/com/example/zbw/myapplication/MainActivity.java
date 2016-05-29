597ab76fbb6f8fc65386f6461f33331ac32417bf
bill <skull9637@gmail.com>
2016-05-29 23:45:41 +0800

    優化指令

--- app/src/main/java/com/example/zbw/myapplication/MainActivity.java
+++ app/src/main/java/com/example/zbw/myapplication/MainActivity.java
@@ -44,12 +44,29 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
 
     //Timing
-    int _second=0;
+    int _second = 0;
+    boolean isScanning = false;
+    boolean screenLock = false;  //螢幕按下，禁止其他感應
     private Handler mHandlerTime = new Handler();
-    boolean isScanning =false;
     //private Button button;
     private TextView hint_text;
     private TextView message;
+
+    private final Runnable timerRun = new Runnable() {
+        public void run() {
+            isScanning = true;
+
+            ++_second; // 經過的秒數 + 1
+            if (_second > 5) {
+                message.setText("");
+                _second = 0;
+                isScanning = false;
+            } else {
+                mHandlerTime.postDelayed(this, 1000);
+            }
+        }
+    };
+
     private TextView time_message;
 
     @Override
@@ -80,77 +97,86 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onDestroy()
-    {
+    public void onDestroy() {
         mHandlerTime.removeCallbacks(timerRun);
         super.onDestroy();
     }
 
+    //設置畫面
     private void findViews() {
         hint_text = (TextView) findViewById(R.id.textView);
         message = (TextView) findViewById(R.id.textView2);
         time_message = (TextView) findViewById(R.id.textView3);
     }
 
+    //設定文字
     private void setText() {
         hint_text.setText("Scan card here");
         message.setText("");
+        time_message.setText(getDateTime());
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
 
 
-        @Override
-        public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
 
-            if (event.getAction() == MotionEvent.ACTION_UP  ) {
+            case MotionEvent.ACTION_UP:
+                // 最後一個點離開
 
-                if(isScanning) return true;
+                Log.i("ACTION_UP", "ACTION_UP");
+                if (isScanning) return true;    //如果前一次的讀卡還在執行中，則阻止感應
                 mHandlerTime.postDelayed(timerRun, 1000);
-            } else if(event.getAction() == MotionEvent.ACTION_DOWN) {
-                int pointerNum = event.getPointerCount();
-                _second=0;
-                //String user_name =
-               // message.setText("get " + pointerNum + " point(s)");
+                break;
+
+            case MotionEvent.ACTION_DOWN:
+                //第一個點按下
+
+                screenLock = true;
+                Log.i("ACTION_DOWN", "ACTION_DOWN");
                 time_message.setText(getDateTime());
-                try {
-                    String result = DBconnector.executeQuery("Select * from USER");
-                    JSONArray jsonArray = new JSONArray(result);
-                    String User_Name="";
-                    for(int i = 0; i < jsonArray.length(); i++) {
-                        JSONObject jsonData = jsonArray.getJSONObject(i);
-                        User_Name = jsonData.getString("User_Name");
-                    }
-                    message.setText("Welcome "+User_Name);
+                break;
 
-                } catch(Exception e) {
-                     Log.e("log_tag", e.toString());
-                }
+            case MotionEvent.ACTION_POINTER_DOWN:
+                //當螢幕上已有一個點，此時再按下其他點
+                Log.i("ACTION_POINTER_DOWN", "ACTION_POINTER_DOWN");
+                _second = 0;
+                int pointerNum = event.getPointerCount();   //觸控點數量
+                Log.i("pointerNum", Integer.toString(pointerNum));
+                String userName;
+                userName = executeQuery("SELECT * FROM `user` WHERE `Nodes` = "+pointerNum);
+                message.setText("welcome "+userName);
+                break;
+        }
+
+        return true;
+    }
+
+    //取得現在時間
+    public String getDateTime() {
+        SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+        Date date = new Date();
+        String strDate = sdFormat.format(date);
+        return strDate;
+    }
+
+    //執行SQL
+    public String executeQuery(String query) {
+        try {
+            Log.i("query",query);
+            String jresult = DBconnector.executeQuery(query);
+            JSONArray jsonArray = new JSONArray(jresult);
+            String firstResult;
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonData = jsonArray.getJSONObject(i);
+                firstResult = jsonData.getString("User_Name");
+                return firstResult;
             }
-            return true;
 
+        } catch (Exception e) {
+            Log.e("log_tag", e.toString());
         }
-
-        public String getDateTime() {
-            SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
-            Date date = new Date();
-            String strDate = sdFormat.format(date);
-            return strDate;
-        }
-
-    private final Runnable timerRun = new Runnable()
-    {
-        public void run()
-        {
-            isScanning=true;
-
-            ++_second; // 經過的秒數 + 1
-            if(_second>5){
-                message.setText("");
-                _second=0;
-                isScanning=false;
-            }else {
-                mHandlerTime.postDelayed(this, 1000);
-            }
-        }
-    };
+        return null;
+    }
 }
